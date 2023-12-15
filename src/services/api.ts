eb82bfea05c7b1e1b2dc37a0a4d7799da8f719aa
larissakoliveira <oliveir5uwm@gmail.com>
2023-12-15 16:07:52 -0300

    fix: changes db baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://hamburkenzie.herokuapp.com/",
+  baseURL: "https://hamburkenzie-db.onrender.com",
 });
 
 export default api;
